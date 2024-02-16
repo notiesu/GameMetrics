d17b2599e403520989e17aa7ecfd46011be3ad18
YvesLangstonMays <yveslangstonmays@gmail.com>
2024-02-16 15:53:37 -0600

    Readability Edits 2

--- StatsGeneration.py
+++ StatsGeneration.py
@@ -28,19 +28,20 @@ for index, year in enumerate(years):
         seasons.append(str(year) + "-" + str(years[index + 1])[2:4])
     except IndexError:
         break
-print(f"Seasons: {seasons}")
 
-lebron_id = [
-    player["id"] for player in players if player["full_name"] == "LeBron James"
-][0]
+"""Much easier to read way of finding the correct id"""
+lebron_id = None
+for player in players:
+    if player["full_name"] == "LeBron James":
+        lebron_id = player["id"]
 
 game_ids = []
 
 for season_num in seasons:
-    cur_season = nba.CommonAllPlayers(
+    current_season = nba.CommonAllPlayers(
         is_only_current_season=0, season=season_num
     ).get_data_frames()[0]
-    current_season = cur_season[current_season["ROSTERSTATUS"] == 1]
+    current_season = current_season[current_season["ROSTERSTATUS"] == 1]
 
     # cur_season = all active players in that season
 
@@ -56,14 +57,22 @@ for season_num in seasons:
             season_type_nullable="Regular Season",
         ).get_data_frames()[0]
         game_ids.append(games["GAME_ID"].values)
+
     # games=all games target player's current team played (including target player DNP)
 
 # Most basic approach: Prediction based off of own player stats
 # X data is going to be completely based off of target player's stats in each box s
 # Making list of game ids
+
 game_ids = np.concatenate(game_ids)
+
+
 print(game_ids)
+
+
 lebron_data = pd.DataFrame()
+
+
 for id in game_ids:
     boxScore = nba.BoxScoreTraditionalV2(game_id=id).get_data_frames()[0]
     lebron_stats = boxScore[boxScore["PLAYER_ID"] == lebron_id]
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from nba_api.stats.endpoints import BoxScoreSummaryV2,BoxScoreTraditionalV2,BoxScoreAdvancedV2
+from nba_api.stats.endpoints import (
+    BoxScoreSummaryV2,
+    BoxScoreTraditionalV2,
+    BoxScoreAdvancedV2,
+)
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 import pandas as pd
 import numpy as np
 import matplotlib as plot
-#import tensorflow as tf
-from nba_api.stats.static import teams,players
+
+# import tensorflow as tf
+from nba_api.stats.static import teams, players
 
 nba_teams = teams.get_teams()
-players=players.get_active_players()
+players = players.get_active_players()
 
 # Select the dictionary for the Celtics, which contains their team ID
-celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
-celtics_id = celtics['id']
+celtics = [team for team in nba_teams if team["abbreviation"] == "BOS"][0]
+celtics_id = celtics["id"]
 from nba_api.stats.endpoints import leaguegamefinder
 
 # Query for games where the Celtics were playing
@@ -20,10 +25,8 @@ gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=celtics_id)
 # The first DataFrame of those returned is what we want.
 games = gamefinder.get_data_frames()[0]
 
-#Trying to get all of the 2019 regular season game data
-games_in_2019=games[games.SEASON_ID == '22019']
-#Create datasets. We are going to try to predict the plus_minus of the next game based on previous game factors.
-#Clean the datasets
-#Ok so now I have the dataset in an excel file to so lets see how this goes
-
-
+# Trying to get all of the 2019 regular season game data
+games_in_2019 = games[games.SEASON_ID == "22019"]
+# Create datasets. We are going to try to predict the plus_minus of the next game based on previous game factors.
+# Clean the datasets
+# Ok so now I have the dataset in an excel file to so lets see how this goes
